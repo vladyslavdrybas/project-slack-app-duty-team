9eaa362b10e3857d40b3241f8c5932020491afc6
Vladyslav Drybas <ukdvvw@gmail.com>
2024-05-14 14:33:33 +0200

    update data mock finder

--- app/tests/mock/RequestDataMock.php
+++ app/tests/mock/RequestDataMock.php
@@ -9,23 +9,39 @@ use Symfony\Component\Finder\Finder;
 class RequestDataMock
 {
     protected readonly string $slackDataPath;
+    protected array $data = [];
 
     public function __construct()
     {
         $this->slackDataPath = dirname(__DIR__) . '/data/slack';
     }
 
-    public function getAddSkillsCommand(): array
+    protected function getData(string $folder, string $fileName, ?string $alias = null): array
     {
+        if (null === $alias) {
+            $alias = $fileName;
+        }
+
+        if (isset($this->data[$alias])) {
+            return $this->data[$alias];
+        }
+
         $finder = new Finder();
-        $finder->in($this->slackDataPath)->files()->name('command*skills*add.json')->contains('/' . CommandList::SkillsAdd->value);
+        $finder->in($folder)->files()->name($fileName);
 
         if ($finder->hasResults()) {
             foreach ($finder as $file) {
-                return json_decode($file->getContents(), true);
+                $this->data[$alias] = json_decode($file->getContents(), true);
+
+                return $this->data[$alias];
             }
         }
 
         return [];
+    }
+
+    public function getAddSkillsCommand(): array
+    {
+        return $this->getData($this->slackDataPath, 'command*skills*add.json', CommandList::SkillsAdd->value);
     }
 }
