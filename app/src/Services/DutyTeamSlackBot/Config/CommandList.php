781b83fd20c338ca610892793a85a64cde89c1cf
Vladyslav Drybas <ukdvvw@gmail.com>
2024-05-19 01:42:07 +0200

    add time offf command stub naming

--- app/src/Services/DutyTeamSlackBot/Config/CommandList.php
+++ app/src/Services/DutyTeamSlackBot/Config/CommandList.php
@@ -8,11 +8,22 @@ enum CommandList: string
     case SkillsAdd = 'skills-add';
     case SkillsRemove = 'skills-remove';
     case SkillsShow = 'skills-show';
+    case TimeOffAdd = 'time-off-add';
+    case TimeOffRemove = 'time-off-remove';
+    case TimeOffShow = 'time-off-show';
 
     public function isSkillsCommand(): bool
     {
         return match ($this) {
             self::SkillsAdd, self::SkillsRemove, self::SkillsShow => true,
+            default => false,
+        };
+    }
+
+    public function isTimeOffCommand(): bool
+    {
+        return match ($this) {
+            self::TimeOffAdd, self::TimeOffRemove, self::TimeOffShow => true,
             default => false,
         };
     }
